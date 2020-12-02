acfbfbc3c5443df09b3319d09680b0255501199e
vo0922 <71069665+vo0922@users.noreply.github.com>
2020-12-02 10:58:02 +0900

    2020-12-02

--- app/src/main/java/com/example/presidentvote/Manager.java
+++ app/src/main/java/com/example/presidentvote/Manager.java
@@ -4,9 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -16,16 +24,26 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Manager extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleApiClient googleApiClient;
+    public final static int REQUES_PHOTO_CODE = 1;
+    ImageView iv_photo;
+    Uri photoUri;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("candidate");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
         setTitle("등록");
 
+
+        iv_photo = (ImageView)findViewById(R.id.photo);
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
@@ -72,5 +90,48 @@ public class Manager extends AppCompatActivity implements GoogleApiClient.OnConn
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    public void selectPhoto(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUES_PHOTO_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUES_PHOTO_CODE) {
+            if (resultCode == RESULT_OK) {
+                photoUri = data.getData();
+                iv_photo.setImageURI(photoUri);
+            } else {
+                Toast.makeText(this, "사진 선택 에러!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void register(View view) {
+        EditText et_name = (EditText)findViewById(R.id.name);
+        String str_name = et_name.getText().toString();
+
+        RadioGroup rg_gender = (RadioGroup)findViewById(R.id.gender);
+        RadioButton rb_gender;
+        String str_gender = "";
+        if(rg_gender.getCheckedRadioButtonId() == R.id.male) {
+            rb_gender = (RadioButton)findViewById(R.id.male);
+            str_gender = rb_gender.getText().toString();
+        }
+        if(rg_gender.getCheckedRadioButtonId() == R.id.female) {
+            rb_gender = (RadioButton)findViewById(R.id.female);
+            str_gender = rb_gender.getText().toString();
+        }
+
+        EditText et_cs = (EditText)findViewById(R.id.cs);
+        String str_cs = et_cs.getText().toString();
+
+        myRef.setValue(str_name);
+        myRef.child(str_name).child("class").setValue(str_cs);
+        myRef.child(str_name).child("gender").setValue(str_gender);
+        //myRef.child(str_name).child("image").setValue();
     }
 }
